80c15cc7cc3f9d1c3d80ed10216e8a5f727b9456
YIRer <xeroonelove@gmail.com>
2016-08-11 18:00:21 +0900

    08111800
    error

--- grunt/js/show_box.js
+++ grunt/js/show_box.js
@@ -1,19 +1,53 @@
 $(function(){
 
   var enter = function(){
-    
-    $(this).children('.img-responsive').stop().slideUp()
+
     $(this).children('.hide_box').stop().slideDown()
   }
   var leave = function(){
 
     $(this).children('.hide_box').stop().slideUp()
-    $(this).children('.img-responsive').stop().slideDown()
+
   }
 
-  $('.gallery').mouseenter(enter)
-  $('.gallery').mouseleave(leave)
+  var click = function(){
+
+    $(this).children('.hide_box').stop().slideToggle()
+
+  }
+
+  // $('.gallery').mouseenter(enter)
+  // $('.gallery').mouseleave(leave)
+  //
+  // $('.gallery').click(click)
+
+  $(window).resize(function(){
+  			var ww = $(this).width()
+        var gh = $('.gallery').height()
+        $('.hide_box').css({height:gh})
+        console.log(gh)
+  			if(990 < ww){
+
+        	$('.gallery').mouseenter(enter);
+  				$('.gallery').mouseleave(leave);
+          $('.gallery').off('click')
 
 
+  			}else if(990 >= ww && 320 < ww){
+          $('.gallery').off('mouseenter');
+          $('.gallery').off('mouseleave');
+          $('.gallery').on('click',click);
 
+        }else{
+
+          $('.hide_box').css({display:'block'})
+          $('.gallery').off('mouseenter');
+          $('.gallery').off('mouseleave');
+          $('.gallery').off('click')
+
+
+        }
+
+  		})
+      $(window).trigger('resize')
 })
