de156b8aecab75a7ef61314a0416b4ef8f837071
YIRer <xeroonelove@gmail.com>
2016-08-23 18:32:41 +0900

    1608231832

--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -1,54 +1,106 @@
 module.exports =  function(grunt){
 
 
-grunt.initConfig({
-  concat: {
-            css: {
-            src: ['css/*.css',],
-            dest: '../theme/assets/css/master.css',
-    },js: {
-    src: ['js/*.js',],
-    dest: '../theme/assets/js/master.js',
-},
-
-
+  grunt.initConfig({
+    concat: {
+              css: {
+              src: ['css/*.css',],
+              dest: '../theme/assets/css/master.css',
+      },js: {
+      src: ['js/*.js',],
+      dest: '../theme/assets/js/master.js',
   },
 
-watch: {
-  css: {
-    files: ['css/*.css'],
-    tasks: ['concat'],
 
-  },js: {
-    files: ['js/*.js'],
-    tasks: ['concat'],
-
-  },
-
-    less: {
-    files: ['less/core.less'],
-    tasks: ['less'],
-
-  },
-},
-
-less: {
-  development: {
-    options: {
-      paths: ['/less']
     },
-    files: {
-      'css/less.css': 'less/core.less'
-    }
+
+  watch: {
+    css: {
+      files: ['css/*.css'],
+      tasks: ['concat'],
+
+    },js: {
+      files: ['js/*.js'],
+      tasks: ['concat'],
+
+    },
+
+      less: {
+      files: ['less/core.less'],
+      tasks: ['less'],
+
+    },
   },
-},
+
+  less: {
+    development: {
+      options: {
+        paths: ['/less']
+      },
+      files: {
+        'css/less.css': 'less/core.less'
+      }
+    },
+  },
 
 
-});
+  });
 
- grunt.loadNpmTasks('grunt-contrib-concat');
- grunt.loadNpmTasks('grunt-contrib-watch');
- grunt.loadNpmTasks('grunt-contrib-less');
+   grunt.loadNpmTasks('grunt-contrib-concat');
+   grunt.loadNpmTasks('grunt-contrib-watch');
+   grunt.loadNpmTasks('grunt-contrib-less');
+
+
+
+//
+// grunt.initConfig({
+//   concat: {
+//             css: {
+//             src: ['css/jeil/*.css',],
+//             dest: '../jeil/css/master.css',
+//     },js: {
+//     src: ['js/jeil/*.js',],
+//     dest: '../jeil/js/master.js',
+// },
+//
+//
+//   },
+//
+// watch: {
+//   css: {
+//     files: ['css/*.css'],
+//     tasks: ['concat'],
+//
+//   },js: {
+//     files: ['js/*.js'],
+//     tasks: ['concat'],
+//
+//   },
+//
+//     less: {
+//     files: ['less/core.less'],
+//     tasks: ['less'],
+//
+//   },
+// },
+//
+// less: {
+//   development: {
+//     options: {
+//       paths: ['/less']
+//     },
+//     files: {
+//       'css/jeil/less.css': 'less/core.less'
+//     }
+//   },
+// },
+//
+//
+// });
+//
+//  grunt.loadNpmTasks('grunt-contrib-concat');
+//  grunt.loadNpmTasks('grunt-contrib-watch');
+//  grunt.loadNpmTasks('grunt-contrib-less');
 
 
 }
